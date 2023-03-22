3e219c2d951e0dac0f771b9f013a0cbb0414bf50
Scott Henshaw <scotthenshaw86@gmail.com>
2023-03-22 14:42:12 +0900

    fixing google button

--- view/signUpSignInTemplate.php
+++ view/signUpSignInTemplate.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= BASE; ?>/css/style.css">
     <script defer src="/js/signUpValidation.js"></script>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+
     <title><?= $title; ?></title>
 </head>
 
